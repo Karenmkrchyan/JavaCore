191c2029537b378d9fea80f84e6c267fad8ac90b
Karenmkrchyan <mkrtchyan1986.km@gmail.com>
2023-08-29 21:05:47 +0300

    Merge branches 'master' and 'master' of https://github.com/Karenmkrchyan/JavaCore
    Signed-off-by: Karenmkrchyan <mkrtchyan1986.km@gmail.com>

---
+++ src/homework1/FigurePainter.java
@@ -0,0 +1,15 @@
+package homework1;
+
+public class FigurePainter {
+    public static void main(String[] args) {
+        String[] stars = {"*", "**", "***", "****", "*****", "******"};
+        int i;
+        for (i = 0; i < 6; i++) {
+            System.out.println(stars[i]);
+
+        }
+
+    }
+
+}
+
