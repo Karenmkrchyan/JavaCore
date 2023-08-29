89fde632891997ed54a1a796efd4fff60de440b1
Karenmkrchyan <mkrtchyan1986.km@gmail.com>
2023-08-29 21:05:47 +0300

    Merge branches 'master' and 'master' of https://github.com/Karenmkrchyan/JavaCore
    Signed-off-by: Karenmkrchyan <mkrtchyan1986.km@gmail.com>

--- src/classwork/chaprar2/For.java
+++ src/classwork/chaprar2/For.java
@@ -4,7 +4,7 @@ public class For {
     public static void main(String[] args) {
         int i;
         for (i = 0; i < 10; i++)
-            System.out.println(i);
+            System.out.println("******");
 
     }
 }
